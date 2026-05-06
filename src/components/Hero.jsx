f350fddf6124c214ad7e828006acffe1a2f49c6d
claireweber12 <claire.weber2004@icloud.com>
2026-05-06 10:22:07 -0500

    Add hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+
+function Hero(){
+    return(
+        <section className="hero">
+            <p className="eyebrow">Software Engineering · AI ·Full-Stack Development</p>
+
+            <h1>Hi, I'm Claire Weber</h1>
+
+            <p className="hero-description">
+                I recently graduated from Auburn University with a Bachelors of Software Engineering
+                I build projects that combine full stack development, AI, and real-world problem solving.
+            </p>
+
+            <div className="hero-buttons">
+                <a href="#projects" className="button primary-button">
+                    View My Work
+                </a>
+
+                <a href="#contact" className="button secondary-button">
+                    Contact Me 
+                </a>
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
