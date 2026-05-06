efde97e9025037b4eb27c0fb22edb16e8f9cd57c
claireweber12 <claire.weber2004@icloud.com>
2026-05-06 11:04:09 -0500

    add About Section

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+
+function About() {
+    return (
+        <section id="about" className="section about">
+            <h2>About Me</h2>
+
+            <p>
+                I recently graduated from Auburn University with a Bachelor of Software Engineering. During my 
+                time at Auburn I also completed a minor in Business, Engineering, and Technology, and an Undergraduate Certificate in Artificial 
+                Intelligence Engineering. I have a deep interest in AI and Machine Learning, full-stack development, and building technology that solves
+                real problems. 
+            </p>
+        </section>
+    );
+}
+
+export default About;
