c5658917fa052a7e0fe19a36eccc3511b479bc65
claireweber12 <claire.weber2004@icloud.com>
2026-05-12 08:47:31 -0500

    update link

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,14 @@
 export const projects = [
 
 {
+    title: "Threaded",
+    category: "Full Stack Web App",
+    description:"A full-stack needlepoint project tracker and inspiration board built with React, Vite, Supabase, and AWS Amplify. Users can create and manage projects, upload images, track thread palettes, add searchable tags, \
+    filter by project status, and mark projects as public or private.",
+    skills: ["React", "AWS Amplify", "Vite"],
+    link: "https://main.d3bty356y6d0wb.amplifyapp.com/",
+},
+{
     title: "RouteCast Weather App",
     category: "Full-stack Web App",
     description: "A weather application using OpenWeatherMap that \
